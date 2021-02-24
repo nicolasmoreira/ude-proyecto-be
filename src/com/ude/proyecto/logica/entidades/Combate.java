f175ae4fb3e1348d497405f60b16a4b820419aff
Mtoriyeim <matias.gallardo.leal@gmail.com>
2021-02-24 01:05:17 -0300

    se crea lista de artilleria

--- src/com/ude/proyecto/logica/entidades/Combate.java
+++ src/com/ude/proyecto/logica/entidades/Combate.java
@@ -59,6 +59,8 @@ public class Combate {
 			torreta.setUbicacionX(depCombustible.getUbicacionX() + baseDistancia);
 			torreta.setUbicacionY(depCombustible.getUbicacionY());
 
+			System.out.println(depCombustible.getUbicacionX() + ' ' + depCombustible.getUbicacionY() );
+			System.out.println(torreta.getUbicacionX() + ' ' + torreta.getUbicacionY() );
 			// aviones
 			ArrayList<Avion> aviones = new ArrayList<Avion>();
 			int cantAviones = Integer.parseInt(p.getProperty("cantAviones"));
@@ -72,16 +74,35 @@ public class Combate {
 				avion.setUbicacionX(depExplosivos.getUbicacionX());
 				
 				aviones.add(avion);// .insert(avion);
-				System.out.println(avion.toString());
+				//System.out.println(avion.toString());
 			}
 
-			System.out.println(aviones.toString());
+			//System.out.println(aviones.toString());
 
+			//creo artilleria
+			ArrayList<Defensa> artillerias = new ArrayList<Defensa>();
+			
+			
+			int cantArtilleria = cantAviones * Integer.parseInt(p.getProperty("multARTILLERIA")); 
+			for (int i = 1; i <= cantArtilleria; i++) {
+				Defensa artilleria = new Defensa(Defensa.TIPO_DEFENSA_ARTILLERIA, p);
+
+				// lo ubico en el campo, es probable que se pisen en la pantalla
+				artilleria.setUbicacionY(artilleria.getUbicacionY() + depCombustible.getUbicacionY() + baseDistancia); //para que no caiga en el rango de la base																							
+				artilleria.setUbicacionX(artilleria.getUbicacionX() + depCombustible.getUbicacionX() + baseDistancia);//para que no caiga en el rango de la base
+				
+				artillerias.add(artilleria);// .insert(avion);
+				//System.out.println(avion.toString());
+			}
+
+			
+			
 			// asigno a player 1
 			player1.setDepositoCombustible(depCombustible);
 			player1.setDepositoExplosivos(depExplosivos);
 			player1.setTorreta(torreta);
 			player1.setAviones(aviones);
+			player1.setArtillerias(artillerias);
 
 			/*----------------------Espejo player 2 ---------------------*/
 
@@ -96,6 +117,9 @@ public class Combate {
 			depExplosivos2.ubicacionEspejar(this.tamanioAncho, depExplosivos.getUbicacionY());
 			torreta2.ubicacionEspejar(this.tamanioAncho, torreta.getUbicacionY());
 
+			System.out.println(depCombustible2.getUbicacionX() + ' ' + depCombustible.getUbicacionY() );
+			System.out.println(torreta2.getUbicacionX() + ' ' + torreta.getUbicacionY() );
+			
 			// mantengo los Y del player 1
 //			DepCombustible2.setUbicacionY(DepCombustible.getUbicacionY());
 //			DepExplosivos2.setUbicacionY(DepExplosivos.getUbicacionY());
@@ -115,11 +139,32 @@ public class Combate {
 				aviones2.add(avion2);// insert(avion2);
 			}
 
+
+			//creo artilleria
+			ArrayList<Defensa> artillerias2 = new ArrayList<Defensa>();
+			
+			
+			//int cantArtilleria = cantAviones * Integer.parseInt(p.getProperty("multARTILLERIA")); 
+			for (int i = 1; i <= cantArtilleria; i++) {
+				//aca adentro espejar cada elemento de la lista anterior
+				Defensa artilleria2 = new Defensa(Defensa.TIPO_DEFENSA_ARTILLERIA, p);
+
+				// lo ubico en el campo, es probable que se pisen en la pantalla
+				artilleria2.setUbicacionY(artilleria2.getUbicacionY() + depCombustible2.getUbicacionY() + baseDistancia); //para que no caiga en el rango de la base																							
+				artilleria2.setUbicacionX(artilleria2.getUbicacionX() + depCombustible2.getUbicacionX() + baseDistancia);//para que no caiga en el rango de la base
+				
+				artillerias2.add(artilleria2);// .insert(avion);
+				
+			}
+			
+			//System.out.println(artillerias2.toString());
+			
 			// asigno a player2
 			player2.setDepositoCombustible(depCombustible2);
 			player2.setDepositoExplosivos(depExplosivos2);
 			player2.setTorreta(torreta2);
 			player2.setAviones(aviones2);
+			player2.setArtillerias(artillerias2);
 
 			// creo jugadores
 			this.jugadores.clear();
